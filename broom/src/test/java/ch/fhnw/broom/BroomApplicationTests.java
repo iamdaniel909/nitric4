cfaad584abb187e768a7300d7b52d6dca094c214
iamdaniel909 <109103812+iamdaniel909@users.noreply.github.com>
2023-06-10 14:44:35 +0000

    changes

--- broom/src/test/java/ch/fhnw/broom/BroomApplicationTests.java
+++ broom/src/test/java/ch/fhnw/broom/BroomApplicationTests.java
@@ -1,10 +1,10 @@
-package ch.fhnw.pizza;
+package ch.fhnw.broom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PizzaApplicationTests {
+class BroomApplicationTests {
 
 	@Test
 	void contextLoads() {
