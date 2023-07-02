6c7477d9dce0bccd92c87e8711f6efadf8ccedbf
iamdaniel909 <109103812+iamdaniel909@users.noreply.github.com>
2023-07-02 17:20:35 +0000

    changes

--- broom/src/main/java/ch/fhnw/broom/business/service/MenuService.java
+++ broom/src/main/java/ch/fhnw/broom/business/service/MenuService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-//import jakarta.validation.Valid;
+import jakarta.validation.Valid;
 
 import ch.fhnw.broom.data.domain.Menu;
 import ch.fhnw.broom.data.domain.Accessory;
